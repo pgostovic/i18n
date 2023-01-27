7df9ce9bfafc7e2ac78cb7da402f02d9821b9281
Patrick Gostovic <pg@phranq.com>
2023-01-27 10:31:43 -0500

    Add react 18 peer dep

--- scripts/postdist.ts
+++ scripts/postdist.ts
@@ -20,7 +20,7 @@ const distPkgJSON = {
   author,
   browser,
   dependencies: { ...dependencies, react: undefined },
-  peerDependencies: { ...peerDependencies, react: '^16.0.0 || ^17.0.0' },
+  peerDependencies: { ...peerDependencies, react: '^16.0.0 || ^17.0.0 || ^18.0.0' },
   description,
   engines,
   keywords,
